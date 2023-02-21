4bb7151d7f98a3a107fa2d129f8639d5de4b72d0
intel <adityakhaladkar98@gmal.com>
2023-02-21 14:08:50 +0530

    arranged activities

---
+++ app/src/main/java/com/example/pbl2/view/auth/SignUp.java
@@ -0,0 +1,16 @@
+package com.example.pbl2.view.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pbl2.R;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
---
+++ app/src/main/java/com/example/pbl2/view/ui/SplashScreen.java
@@ -0,0 +1,16 @@
+package com.example.pbl2.view.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pbl2.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
