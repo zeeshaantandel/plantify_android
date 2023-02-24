1d4281f61683b46150aee826fd54b1ed43e36e85
zeeshaantandel <zeeshaan143tandel@gmail.com>
2023-02-25 00:25:12 +0530

    added sign-in page background image

--- app/src/main/java/com/example/pbl2/view/auth/SignIn.java
+++ app/src/main/java/com/example/pbl2/view/auth/SignIn.java
@@ -3,14 +3,21 @@ package com.example.pbl2.view.auth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.example.pbl2.R;
 
 public class SignIn extends AppCompatActivity {
 
+    /*RelativeLayout bg1;*/
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        /*bg1=findViewById(R.id.bg1);
+        bg1.setClipToOutline(true);*/
     }
 }
